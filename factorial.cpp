6a50625d5b5140bedcee46cbec217581dc9adc45
Riya Arora <arorariya2003@gmail.com>
2023-08-20 15:27:51 +0530

    modified factorial.cpp
    Added negative number check to factorial.cpp and temporary.md file is now added to .gitignore

--- factorial.cpp
+++ factorial.cpp
@@ -1,6 +1,13 @@
 #include "functions.h"
 
 int factorial(int n){
+
+    while (n < 0){
+        int x;
+        cin >> x;
+        n = x;
+    }
+
     if(n!=1){
 	return(n * factorial(n-1));
     }
