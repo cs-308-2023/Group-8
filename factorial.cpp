5623b9c0647762b5e00cd95480f23dfa10e82d2d
Riya Arora <arorariya2003@gmail.com>
2023-08-21 16:08:26 +0530

    printf to cout and other changes

--- factorial.cpp
+++ factorial.cpp
@@ -1,17 +1,6 @@
 #include "functions.h"
-#include <iostream>
-using namespace std;
 
 int factorial(int n){
-
-    // Negative number check
-    while (n < 0){
-        int x;
-        cout << "Enter valid positive number\n"; 
-        cin >> x; // Taking new input
-        n = x;
-    }
-
     if(n!=1){
 	return(n * factorial(n-1));
     }
