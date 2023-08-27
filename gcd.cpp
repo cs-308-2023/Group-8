f28031800da57637b54575327491c94efb98d4a8
Riya Arora <arorariya2003@gmail.com>
2023-08-27 17:29:38 +0530

    recursive gcd function added to gcd.cpp

--- gcd.cpp
+++ gcd.cpp
@@ -1,11 +1,20 @@
 #include "functions.h"
 
-int gcd(int x, int y){
-    while (x && y){
+int gcd_recursive(int x, int y){
+    if (x == 0) return y; // Recursion base case
+    else return gcd_recursive(x, y % x); // Recursive becuase the function calls itself
+}
+
+int gcd_non_recursive(int x, int y){
+    while (x && y){ // Previous iterative implementation
         if (x > y) x = x % y;
         else y = y % x;
     }
 
-    if (x == 0) return y;
-    else return x;
+    if (x == 0) {
+        return y;
+    }
+
+    return x;
 }
+
