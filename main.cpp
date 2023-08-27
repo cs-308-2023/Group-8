4d0ea5aa56706dc8afd8282134add016381850d7
Riya Arora <arorariya2003@gmail.com>
2023-08-27 17:46:38 +0530

    branch add-rec-function merged

--- main.cpp
+++ main.cpp
@@ -5,5 +5,11 @@ int main(){
     print_hello();
     cout << endl;
     cout << "The factorial of 5 is " << factorial(5) << endl;
+
+    int x = 56;
+    int y = 98;
+    cout << "GCD IMPLEMENTATION:\n";
+    cout << "The GCD of " << x << " and " << y << " is " << gcd_non_recursive(x, y) << endl;
+
     return 0;
 }
